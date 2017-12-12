e24b9ecfd1d143c13c92b02fca78f37564a37578
Sathya Moorthy S <sathya.sabagaja@gmail.com>
2017-12-13 00:50:47 +0530

    Added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const http = require('http')
+const socketIO = require('socket.io')
 const bodyParser = require('body-parser');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000 ;
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket)=> {
+    console.log('New user connected')
+    socket.on('disconnect', ()=>{
+        console.log('User disconnected')
+    })
+})
 
 app.use(express.static(publicPath));
-app.use(bodyParser.json());
-
-
-
-app.listen(port, ()=> {
+server.listen(port, ()=> {
     console.log(`Chat App listening at ${port}`)
 })
